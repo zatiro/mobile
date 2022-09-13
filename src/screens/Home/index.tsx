be05718a0aefbf863b316bd7f9aa86762f5a7d04
Tiago Cardoso <40040933+zatiro@users.noreply.github.com>
2022-09-13 17:08:17 -0300

    Completing frontend components and configs

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,35 @@
+import { View, Image, FlatList } from 'react-native';
+
+import { styles } from './styles';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+
+import { GAMES } from '../../utils/games';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      ></Image>
+
+      <Heading title="Encontre seu duo!" subtitle='Selecione o game que deseja jogar...'></Heading>
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={item => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => {
+          return (<GameCard data={item} />)
+        }}
+      />
+
+
+    </View>
+  );
+}
